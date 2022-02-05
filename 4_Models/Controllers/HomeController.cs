eb92ace35182d2d698398b22f751abb1f5c775cd
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-02-05 13:58:54 +0300

    Model bind to action

---
+++ 4_Models/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _4_Models.Models;
+
+namespace _4_Models.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // Serverdan sayfa veya action çağırdığıdan bu get işlemidir..
+
+        // sayfadan action'a birşey gönderdiğinde bu post işlemidir.. (gizli veri gönderimi)
+
+        [HttpGet] // sadece get edilebilir...
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost] // sadece post edilebilir...
+        public IActionResult Index(User model)
+        {
+            string gelenUserName = model.UserName;
+            string password = model.Password;
+
+            return View();
+        }
+    }
+}
